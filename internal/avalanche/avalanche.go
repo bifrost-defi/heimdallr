b564b7ba53fa6d982fd7dd9abf9fe7f89601434c
AlexeySadkovich <sadkovich01@mail.ru>
2022-02-26 18:15:47 +0300

    feat(avalanche): add usdc unlock call

--- internal/avalanche/avalanche.go
+++ internal/avalanche/avalanche.go
@@ -85,47 +85,71 @@ func (a *Avalanche) Subscribe(ctx context.Context) (*Subscription, error) {
 }
 
 func (a *Avalanche) UnlockAVAX(ctx context.Context, user string, amount *big.Int) (string, *big.Int, error) {
-	privateKey, err := crypto.HexToECDSA(a.privateKey)
+	opts, err := a.createTransactor(ctx)
 	if err != nil {
-		return "", nil, fmt.Errorf("invalid private key: %w", err)
+		return "", nil, err
 	}
-
-	publicKey := privateKey.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		return "", nil, fmt.Errorf("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
-	}
-
-	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-	nonce, err := a.client.PendingNonceAt(ctx, fromAddress)
-	if err != nil {
-		return "", nil, fmt.Errorf("pending nonce: %w", err)
-	}
-
-	gasPrice, err := a.client.SuggestGasPrice(ctx)
-	if err != nil {
-		return "", nil, fmt.Errorf("suggest gas price: %w", err)
-	}
-
-	chainID, err := a.client.ChainID(ctx)
-	if err != nil {
-		return "", nil, fmt.Errorf("get chain id: %w", err)
-	}
-
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
-	if err != nil {
-		return "", nil, fmt.Errorf("new keyed transaction: %w", err)
-	}
-	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0)
-	auth.GasLimit = uint64(300000)
-	auth.GasPrice = gasPrice
 
 	userAddress := common.HexToAddress(user)
-	tx, err := a.locker.UnlockAVAX(auth, userAddress, amount)
+	tx, err := a.locker.UnlockAVAX(opts, userAddress, amount)
 	if err != nil {
 		return "", nil, fmt.Errorf("call unlockAVAX: %w", err)
 	}
 
 	return tx.Hash().Hex(), tx.Cost(), nil
+}
+
+func (a *Avalanche) UnlockUSDC(ctx context.Context, user string, amount *big.Int) (string, *big.Int, error) {
+	opts, err := a.createTransactor(ctx)
+	if err != nil {
+		return "", nil, err
+	}
+
+	userAddress := common.HexToAddress(user)
+	tx, err := a.locker.UnlockUSDC(opts, userAddress, amount)
+	if err != nil {
+		return "", nil, fmt.Errorf("call unlockAVAX: %w", err)
+	}
+
+	return tx.Hash().Hex(), tx.Cost(), nil
+}
+
+func (a *Avalanche) createTransactor(ctx context.Context) (*bind.TransactOpts, error) {
+	privateKey, err := crypto.HexToECDSA(a.privateKey)
+	if err != nil {
+		return nil, fmt.Errorf("invalid private key: %w", err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := a.client.PendingNonceAt(ctx, fromAddress)
+	if err != nil {
+		return nil, fmt.Errorf("pending nonce: %w", err)
+	}
+
+	gasPrice, err := a.client.SuggestGasPrice(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("suggest gas price: %w", err)
+	}
+
+	chainID, err := a.client.ChainID(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("get chain id: %w", err)
+	}
+
+	opts, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	if err != nil {
+		return nil, fmt.Errorf("new keyed transactor: %w", err)
+	}
+	opts.Nonce = big.NewInt(int64(nonce))
+	opts.Value = big.NewInt(0)
+	opts.GasLimit = uint64(300000)
+	opts.GasPrice = gasPrice
+
+	return opts, nil
 }
